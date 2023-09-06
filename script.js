0f6518c0a20661e05d1f6200c24e0d8e97835014
hansenlau <141025610+hansenlau@users.noreply.github.com>
2023-09-06 17:52:55 +0700

    First attempt

---
+++ script.js
@@ -0,0 +1,11 @@
+getComputerChoice(){
+    let random = Math.random();
+    if(random < 0.33) {
+        console.log("Rock");}
+    else if(random < 0.66 && random >= 0.33) {
+        console.log("Scissors");}
+    else console.log("Paper");
+};
+
+getComputerChoice();
+console.log(random);
