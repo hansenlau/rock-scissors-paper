66f612bd1e70febca212cbe732ae6ed1132792e9
hansenlau <sen_lau@hotmail.com>
2023-09-06 18:08:34 +0700

    Computer Random Number Done

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-getComputerChoice(){
+function getComputerChoice() {
     let random = Math.random();
     if(random < 0.33) {
         console.log("Rock");}
     else if(random < 0.66 && random >= 0.33) {
         console.log("Scissors");}
     else console.log("Paper");
+    console.log(random);
 };
 
 getComputerChoice();
-console.log(random);
