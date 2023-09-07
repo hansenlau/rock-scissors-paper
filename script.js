ee946cc759cd75d8e22f2883cfa0337e717c3c22
hansenlau <141025610+hansenlau@users.noreply.github.com>
2023-09-07 17:03:34 +0700

    Declare playRound function

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
 function getComputerChoice() {
     let random = Math.random();
     if(random < 0.33) {
-        console.log("Rock");}
+        return "Rock"
+    }
     else if(random < 0.66 && random >= 0.33) {
-        console.log("Scissors");}
-    else console.log("Paper");
-    console.log(random);
+        return "Scissors"
+    }
+    else return "Paper"
+        };
+
+//console.log(getComputerChoice());
+
+function playRound(playerSelection,computerSelection) {
+    
+
+    if(playerSelection === computerSelection) return "it's a tie!";
+    if(playerSelection === "rock" && computerSelection === "scissors") return "you win!";
+    else if(playerSelection === "rock" && computerSelection === "paper") return "you lose!";
+    else if(playerSelection === "scissors" && computerSelection === "paper") return "you win!";
+    else if(playerSelection === "scissors" && computerSelection === "rock") return "you lose!";
+    else if(playerSelection === "paper" && computerSelection === "scissors") return "you lose!";
+    else if (playerSelection === "paper" && computerSelection === "rock") return "you win!";
+    else return "oops error";
 };
 
-getComputerChoice();
+const playerSelection = prompt("Rock, Scissors, or Paper?").toLowerCase();
+const computerSelection = getComputerChoice().toLowerCase();
+
+console.log("Your choice is "+ playerSelection + ", computer choice is "+ computerSelection)
+console.log(playRound(playerSelection,computerSelection));
+
